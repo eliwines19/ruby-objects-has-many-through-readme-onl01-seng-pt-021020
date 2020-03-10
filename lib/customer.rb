bef765b31173d68058c87ab01ed6c06b53e53599
Elijah wines <elijahwines84@yahoo.com>
2020-03-10 14:10:34 +0000

    Automatically backed up by Learn

--- lib/customer.rb
+++ lib/customer.rb
@@ -12,7 +12,7 @@ class Customer
     @@all
   end
 
-  def new_meal
-    Meal.new(self, waiter, total, tip=0)
+  def new_meal(waiter, total, tip=0)
+    Meal.new(self, waiter, total, tip)
   end
 end
