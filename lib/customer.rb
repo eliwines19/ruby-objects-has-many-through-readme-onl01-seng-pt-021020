c2c472c91533a0321d77ba1b219ab6d153167d74
Elijah wines <elijahwines84@yahoo.com>
2020-03-10 14:04:43 +0000

    Automatically backed up by Learn

--- lib/customer.rb
+++ lib/customer.rb
@@ -1,3 +1,16 @@
 class Customer
+  @@all = []
+  attr_accessor :name, :age
+
+  def initialize(name, age)
+    @name = name
+    @age = age
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
   
 end
--- lib/waiter.rb
+++ lib/waiter.rb
@@ -1,3 +1,15 @@
 class Waiter
+  @@all = []
+  attr_accessor :name, :years_exp
 
+  def initialize(name, years_exp)
+    @name = name
+    @years_exp = years_exp
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+  
 end
