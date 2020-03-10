70f4c67f3748c6291e24e75dae5945ed9b9d2eed
Elijah wines <elijahwines84@yahoo.com>
2020-03-10 14:21:29 +0000

    Automatically backed up by Learn

--- lib/waiter.rb
+++ lib/waiter.rb
@@ -22,6 +22,11 @@ class Waiter
     end
   end
 
-  
+  def best_tipper
+    best_tipped_meal = meal.max do |meal_a, meal_b|
+      meal_a.tip <=> meal_b.tip
+    end
+    best_tipped_meal.customer 
+  end
 
 end
